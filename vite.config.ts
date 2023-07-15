dde19c87622f6e58e2439dff6177a6a56fe657d1
Davide Ghiotto <davide.ghiotto@bitrock.it>
2023-07-15 17:32:52 +0200

    test icons + manifest

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,29 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      manifest: {
+        name: "Spliteam",
+        short_name: "spliteam",
+        description: "A simple app to split your team",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/images/ios/192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/ios/512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
