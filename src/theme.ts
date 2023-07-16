a4ad350510b182b8590d10c0c5c4507641251dcd
Davide Ghiotto <davide.ghiotto@bitrock.it>
2023-07-16 12:26:22 +0200

    update colors

--- src/theme.ts
+++ src/theme.ts
@@ -2,11 +2,18 @@ import { GlobalThemeOverrides } from "naive-ui";
 
 export const theme: GlobalThemeOverrides = {
   common: {
-    successColor: "#FF8800",
-    primaryColor: "#FF8800",
+    successColor: "#F29544",
+    primaryColor: "#F29544",
   },
   Button: {
-    colorSuccess: "#FF8800",
-    colorPrimary: "#FF8800",
+    colorSuccess: "#F29544",
+    colorPrimary: "#F29544",
+    colorHoverSuccess: "#A64E46",
+    colorHoverPrimary: "#A64E46",
+    colorPressedSuccess: "#A64E46",
+    colorPressedPrimary: "#A64E46",
+    borderSuccess: "#A64E46",
+    borderHoverSuccess: "#A64E46",
+    borderPressedSuccess: "#A64E46",
   },
 };
